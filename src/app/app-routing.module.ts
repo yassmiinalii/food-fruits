a8fe2ebf7ac19141fd00acc18772e2f03122d127
yassmiinalii <yasmiiinaliii@gmail.com>
2023-04-05 10:17:32 +0200

    init routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./modules/home/home.module').then(
+        (a) => a.HomeModule
+      ),
+  },
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./modules/home/home.module').then(
+        (a) => a.HomeModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -1,7 +1,16 @@
+import { CategoryListSectionComponent } from './sections/category-list-section/category-list-section.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',
+  component: HomePageComponent
+  },
+  { path: 'categories',
+  component: CategoryListSectionComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
